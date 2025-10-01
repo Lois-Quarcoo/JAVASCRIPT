4eb3cdf633515277e6cf59e2a2efb666a23bafae
Lois <loisitoswift@gmail.com>
2025-10-01 12:42:53 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+// let box = "laptop"
+
+//operators + - /
+// let a = 4
+// let b = 5 
+
+// console.log(++a) 
+// console.log(--b)
+
+//comparison operators 
+// let a = 4
+// let b = 5 
+// console.log(a==b)
+// console.log(a===b)
+
+
+// &&(AND)
+//  let a = 7
+//  let b = 5
+//  let c =8
+//  let d = 4
+//  console.log(a>b && c>d)
+
+
+ // ||(OR)
+
+//  let a = 7
+//  let b = 5
+//  let c =8
+//  let d = 4
+
+// console.log(b>a || c>d )
+
+ // !(NOT)
+//  let a = 7
+//  let b = 5
+
+//  let answer = !(a<b)
+//  console.log (answer)
+
+
+// strings
+
+let a = "lois"
+let ds ="yaw"
+let id = "her"
+let name ="katy"
+
+//numbers
+let t = 5
+let n = 6
+let g = 7
+
+//boolean 
+
+let katy = true
+let h = false
+
